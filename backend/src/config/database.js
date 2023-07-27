388f6d70b7f1c81654fb7bfed5cc1d08b44be6ee
dewandra <75962411+dewandra@users.noreply.github.com>
2023-07-27 08:37:27 +0700

    Delete backend directory

--- backend/src/config/database.js
+++
@@ -1,9 +0,0 @@
-import { Sequelize } from "sequelize";
-import "dotenv/config";
-
-const db = new Sequelize("bakorwil3_malang", "root", "", {
-  host: process.env.DB_HOST,
-  dialect: "mysql",
-});
-
-export default db;
