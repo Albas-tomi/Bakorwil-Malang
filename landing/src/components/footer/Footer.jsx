a2cd3cb1e4f0fdf12d31c3cb4f3f4d9ed46477f1
AlfinAfifan <agusalfin1@gmail.com>
2023-08-05 18:53:00 +0700

    Link Bakorwil Footer

--- landing/src/components/footer/Footer.jsx
+++ landing/src/components/footer/Footer.jsx
@@ -1,4 +1,4 @@
-import { FaLocationDot } from 'react-icons/fa6';
+import { FaEnvelope, FaGlobe, FaLocationDot, FaPhone } from 'react-icons/fa6';
 
 import LogoJatim from '../../assets/image/logopemrov-png.png';
 import LogoJdihn from '../../assets/image/jdihn.png';
@@ -8,8 +8,9 @@ import LogoLapor from '../../assets/image/lapor.png';
 const Footer = () => {
   return (
     <div>
+      {/* Pemprov Jatim */}
       <div className="footer mt-10 px-5 bg-dark text-white pb-5">
-        <h1 className="font-semibold text-lg mx-auto mt-10">PEMPROV JATIM</h1>
+        <h1 className="font-medium text-lg mx-auto mt-10">PEMPROV JATIM</h1>
         <div className="isi mx-auto">
           <div className="grid grid-cols-2 gap-x-16 gap-y-7 mx-auto items-center">
             <img src={LogoJatim} alt="" className="w-20" />
@@ -17,31 +18,66 @@ const Footer = () => {
             <img src={LogoLpse} alt="" className="w-20" />
             <img src={LogoLapor} alt="" className="w-20" />
           </div>
-          <h1 className="font-semibold text-lg  mx-auto mt-10">KONTAK KAMI</h1>
-          <div className="isi mx-auto">
-            <div className="flex gap-3 text-sm mt-5">
+
+          {/* Kontak Kami */}
+          <h1 className="font-medium text-lg  mx-auto mt-10">KONTAK KAMI</h1>
+          <div className="isi mx-auto flex flex-col gap-4 text-base">
+            <a href="#" className="flex gap-3 mt-5">
               <FaLocationDot className="text-lg self-center" />
               <div className="font-light">
                 Jl. Simpang Ijen No. 2 Oro-oro Dowo,
                 <br />
                 Kecamatan Klojen Kota Malang
-                <div className="h-2 w-full bg-text"></div>
+                <div className="h-[2px] w-full bg-white"></div>
               </div>
-            </div>
-            <div className="flex gap-3 text-sm">
-              <FaLocationDot className="text-lg self-center" />
+            </a>
+            <a href="#" className="flex gap-3">
+              <FaPhone className="text-lg self-center" />
               <div className="font-light">
                 (0341) 551321
-                <hr />
+                <div className="h-[2px] w-full bg-white"></div>
               </div>
-            </div>
-            <div className="flex gap-3 text-sm">
-              <FaLocationDot className="text-lg self-center" />
+            </a>
+            <a href="#" className="flex gap-3">
+              <FaEnvelope className="text-lg self-center" />
               <div className="font-light">
                 bakorwil3provjatim.dimalang@gmail.com
-                <hr />
+                <div className="h-[2px] w-full bg-white"></div>
               </div>
-            </div>
+            </a>
+          </div>
+
+          {/* Link Bakorwil */}
+          <h1 className="font-medium text-lg  mx-auto mt-10">BAKORWIL JATIM</h1>
+          <div className="isi mx-auto flex flex-col gap-4 text-base">
+            <a href="#" className="flex gap-3 mt-5">
+              <FaGlobe className="text-lg self-center" />
+              <div className="font-light">
+                Bakorwil Madiun
+                <div className="h-[2px] w-full bg-white"></div>
+              </div>
+            </a>
+            <a href="#" className="flex gap-3 mt-5">
+              <FaGlobe className="text-lg self-center" />
+              <div className="font-light">
+                Bakorwil Bojonegoro
+                <div className="h-[2px] w-full bg-white"></div>
+              </div>
+            </a>
+            <a href="#" className="flex gap-3 mt-5">
+              <FaGlobe className="text-lg self-center" />
+              <div className="font-light">
+                Bakorwil Pamekasan
+                <div className="h-[2px] w-full bg-white"></div>
+              </div>
+            </a>
+            <a href="#" className="flex gap-3 mt-5">
+              <FaGlobe className="text-lg self-center" />
+              <div className="font-light">
+                Bakorwil Jember
+                <div className="h-[2px] w-full bg-white"></div>
+              </div>
+            </a>
           </div>
         </div>
       </div>
