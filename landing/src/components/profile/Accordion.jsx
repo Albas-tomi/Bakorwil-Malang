f493b206f9183ffd674ae3d64956052fadd7099a
D <dewandrarb@gmail.com>
2023-08-07 14:41:26 +0700

    update

--- landing/src/components/profile/Accordion.jsx
+++ landing/src/components/profile/Accordion.jsx
@@ -1,34 +1,19 @@
 import React from "react";
+import { FaFeather } from "react-icons/fa";
 
 export const Accordion = () => {
   return (
     <div section="accordion" className="bg-second flex flex-col gap-y-4 p-5">
       <div className="collapse collapse-arrow bg-base-200">
         <input type="radio" name="my-accordion-2" checked="checked" />
-        <div className="collapse-title text-sm">Sejarah Bakorwil</div>
+        <div className="collapse-title text-sm font-bold text-second flex items-center gap-2">
+          <span>
+            <FaFeather />
+          </span>
+          Sejarah Bakorwil
+        </div>
         <div className="collapse-content">
           <p className="text-sm">hello sejarah bakorwil</p>
-        </div>
-      </div>
-      <div className="collapse collapse-arrow bg-base-200">
-        <input type="radio" name="my-accordion-2" checked="checked" />
-        <div className="collapse-title text-sm">Tujuan & Sasaran</div>
-        <div className="collapse-content">
-          <p className="text-sm">hello tujuan & sasaran</p>
-        </div>
-      </div>
-      <div className="collapse collapse-arrow bg-base-200">
-        <input type="radio" name="my-accordion-2" checked="checked" />
-        <div className="collapse-title text-sm">Struktur Bakorwil</div>
-        <div className="collapse-content">
-          <p className="text-sm">hello struktur bakorwil</p>
-        </div>
-      </div>
-      <div className="collapse collapse-arrow bg-base-200">
-        <input type="radio" name="my-accordion-2" checked="checked" />
-        <div className="collapse-title text-sm">Wilayah Kerja</div>
-        <div className="collapse-content">
-          <p className="text-sm">hello wilayah kerja</p>
         </div>
       </div>
     </div>
--- landing/src/pages/Profile.jsx
+++ landing/src/pages/Profile.jsx
@@ -3,10 +3,25 @@ import { Accordion } from "../components/profile/Accordion";
 
 export const Profile = () => {
   return (
-    <div id="profile ">
-      <div className="grid grid-cols-2 mb-5 px-5">
-        <div className="bg-primer text-center">1</div>
-        <div className="bg-birumuda2 text-center">2</div>
+    <div id="profile">
+      <div className="grid justify-items-center mb-5 px-5 md:grid-cols-2">
+        <div className="text-center md:order-last">
+          <img
+            src="https://cdn-icons-png.flaticon.com/512/3135/3135715.png"
+            alt="foto"
+            className="w-40"
+          />
+          <h1 className="font-bold underline">Drs. Budi Santosa</h1>
+          <p className="text-sm text-text">Kepala Bakorwil Malang</p>
+        </div>
+        <div className="mt-5 text-center md:self-center">
+          <h1 className="font-bold text-primer">APA ITU BAKORWIL ? </h1>
+          <p className="text-sm text-text">
+            Bakorwil merupakan unsur penunjang kepala daerah, dipimpin oleh
+            seorang kepala, yang berada di bawah dan bertanggung jawab kepada
+            Gubernur melalui Sekretaris Daerah.
+          </p>
+        </div>
       </div>
       <Accordion />
     </div>
