5d15b3a74e17dbdbf89a9cd5cbb36207df39ba5a
Albastomi <albustomi679@gmail.com>
2023-08-14 09:58:54 +0700

    test

--- landing/src/components/beranda/Slider.jsx
+++ landing/src/components/beranda/Slider.jsx
@@ -1,64 +1,29 @@
 import React from "react";
 
+import Data from "../../Data.json";
+
 const Slider = () => {
   return (
-    <div>
+    <>
       <div className="carousel w-full">
-        <div id="slide1" className="carousel-item relative w-full">
-          <img src="https://placehold.co/1000x600" className="w-full" />
-          <div className="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
-            <a href="#slide4" className="btn btn-circle">
-              ❮
-            </a>
-            <a href="#slide2" className="btn btn-circle">
-              ❯
-            </a>
+        {Data.map((dataSlider, idx) => (
+          <div id={idx} className="carousel-item relative w-full">
+            <img
+              src={dataSlider.image}
+              className="w-full h-56 md:h-96 lg:h-screen object-cover"
+            />
+            <div className="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
+              <a href={`#${idx - 1}`} className="btn btn-circle">
+                ❮
+              </a>
+              <a href={`#${idx + 1}`} className="btn btn-circle">
+                ❯
+              </a>
+            </div>
           </div>
-        </div>
-        <div id="slide2" className="carousel-item relative w-full">
-          <img
-            src="/images/stock/photo-1609621838510-5ad474b7d25d.jpg"
-            className="w-full"
-          />
-          <div className="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
-            <a href="#slide1" className="btn btn-circle">
-              ❮
-            </a>
-            <a href="#slide3" className="btn btn-circle">
-              ❯
-            </a>
-          </div>
-        </div>
-        <div id="slide3" className="carousel-item relative w-full">
-          <img
-            src="/images/stock/photo-1414694762283-acccc27bca85.jpg"
-            className="w-full"
-          />
-          <div className="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
-            <a href="#slide2" className="btn btn-circle">
-              ❮
-            </a>
-            <a href="#slide4" className="btn btn-circle">
-              ❯
-            </a>
-          </div>
-        </div>
-        <div id="slide4" className="carousel-item relative w-full">
-          <img
-            src="/images/stock/photo-1665553365602-b2fb8e5d1707.jpg"
-            className="w-full"
-          />
-          <div className="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
-            <a href="#slide3" className="btn btn-circle">
-              ❮
-            </a>
-            <a href="#slide1" className="btn btn-circle">
-              ❯
-            </a>
-          </div>
-        </div>
+        ))}
       </div>
-    </div>
+    </>
   );
 };
 
