45452c50b6da5c70ef52fc223e594c0485b87cdf
D <dewandrarb@gmail.com>
2023-08-14 15:18:11 +0700

    update ppid 30%

--- landing/src/components/PPID/Gambarprosedur.jsx
+++ landing/src/components/PPID/Gambarprosedur.jsx
@@ -10,23 +10,23 @@ const Gambarprosedur = () => {
           </h3>
         </div>
         <img
-          src="https://bakorwilmalang.jatimprov.go.id/images/struktur_organisasi/3db4ad8555afee7d6e682d8dacd65590.png"
+          src="https://bakorwilmalang.jatimprov.go.id/images/ppid_layanan_informasi/bce0c48d1132df333aa97840cc0d8fa5.jpg"
           alt=""
         />
       </div>
-      <div className="my-6 md:rotate-90 md:w-56">
+      <div className="my-6 md:rotate-90 md:w-[23rem]">
         <div className="h-1 rounded w-40 mx-auto bg-second md:w-40"></div>
       </div>
       <div className="md:grid items-center relative">
-        <div class="absolute bottom-0 left-0 right-0 px-4 py-2 bg-[#262626] opacity-70 md:py-4 md:bottom-[120px]">
-          <h3 class="text-sm text-center text-white font-bold md:text-lg">
-          Alur Penyelesaian Sengketa Informasi Melalui Komisi Konfirasi
+        <div class="absolute bottom-0 left-0 right-0 px-4 py-2 bg-[#262626] opacity-70 md:py-4 md:bottom-[90px]">
+          <h3 class="text-sm text-center text-white font-bold md:text-xs">
+            Alur Penyelesaian Sengketa Informasi Melalui Komisi Konfirasi
           </h3>
         </div>
-        <img
-          src="https://bakorwilmalang.jatimprov.go.id/images/struktur_organisasi/3db4ad8555afee7d6e682d8dacd65590.png"
-          alt=""
-        />
+          <img
+            src="https://bakorwilmalang.jatimprov.go.id/images/ppid_layanan_informasi/281458f47b16c96cea8d39a1ff12c035.jpg"
+            alt=""
+          />
       </div>
     </>
   );
