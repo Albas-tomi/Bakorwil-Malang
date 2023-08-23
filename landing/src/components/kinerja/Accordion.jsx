7b594eb30b0a20b83af1f688d6247f2491c6bfcb
AlfinAfifan <agusalfin1@gmail.com>
2023-08-23 15:17:58 +0700

    Sejarah, tabel PPID

--- landing/src/components/kinerja/Accordion.jsx
+++ landing/src/components/kinerja/Accordion.jsx
@@ -8,11 +8,10 @@ const Accordion = () => {
         <input type="radio" name="my-accordion-1" checked="checked" />
         <div className="collapse-title text-sm font-bold text-second flex items-center gap-2 md:text-lg">Sekertariat</div>
         <div className="collapse-content">
-          <p className="text-sm md:text-base">
-            1. Rapat koordinasi, fasilitasi, dan menyinergikan tugas dan fungsi Bakorwil III Malang dengan unit pelakasana teknis dan cabang Dinas Pendidikan di seluruh wilayah kerja Bakowril III Malang tahun 2021.
-            <br />
-            2. Sinkronisasi, monitoring, dan evaluasi keterisian data dalam Sistem Informasi Pemerentihan Daerah (SIPD) di seluruh Kabupaten dan Kota se-wilayah kerja Bakowril III Malang tahun 2021.
-          </p>
+          <ul className="list-decimal ml-5 mb-8 text-sm md:text-base">
+            <li>Rapat koordinasi, fasilitasi, dan menyinergikan tugas dan fungsi Bakorwil III Malang dengan unit pelakasana teknis dan cabang Dinas Pendidikan di seluruh wilayah kerja Bakowril III Malang tahun 2021.</li>
+            <li>Sinkronisasi, monitoring, dan evaluasi keterisian data dalam Sistem Informasi Pemerentihan Daerah (SIPD) di seluruh Kabupaten dan Kota se-wilayah kerja Bakowril III Malang tahun 2021.</li>
+          </ul>
         </div>
       </div>
       {/* ================== BIDANG PEMERINTAHAN ================== */}
@@ -20,21 +19,20 @@ const Accordion = () => {
         <input type="radio" name="my-accordion-1" checked="checked" />
         <div className="collapse-title text-sm font-bold text-second flex items-center gap-2 md:text-lg">Bidang Pemerintahan</div>
         <div className="collapse-content">
-          <div className="text-sm md:text-base">
-            1. Kegiatan koordinasi. fasilitasi, monitoring dan evaluasi kerja sama Blitar Raya, Pemerintah Kabupaten Malang dan Pemerintah Kabupaten Tulungagung dalam rangka mendukung Peraturan Presiden No. 80 Tahun 2019 tentang Proyek
-            Selingkar Wilis dan Jalan Lintas Selatan (JLS).
-            <br />
-            2. Rekomendasi Inisiasi percepatan pelaksanaan kesepakatan bersama pembangunan Bromo Tengger Semeru meliputi Pemerintah Provinsi Jawa Timur, Pemerintah Kota Batu, Pemerintah Kabupaten dan Kota Malang, Pemerintah Kabupaten dan
-            Kota Pasuruan, Pemerintah Kabupaten dan Kota Probolinggo dan Kabupaten Lumajang.
-            <br />
-            3. Kegiatan koordinasi, fasilitasi, monitoring dan evaluasi kebencanaan daerah hulu dan hilir seputar Daerah Aliran Sungai (DAS) Brantas di Malang Raya.
-            <br />
-            4. Kegiatan koordinasi, fasilitasi, monitoring dan evaluasi pembinaan kearsipan mendukung pertanggungjawaban pengelolaan dana desa dalam penanganan COVID-19.
-            <br />
-            5. Kegiatan koordinasi, fasilitasi, monitoring dan evaluasi kesiapsiagaan menghadapi bencana hidrometeorologi (banjir, tanah longsor, dIl.) sewilayah kerja Bakorwil III Malang.
-            <br />
-            6. Kegiatan koordinasi, fasilitasi, monitoring dan evaluasi sistem kependudukan yang terintegrasi dengan Satu Data Jawa Timur untuk seluruh masyarakat sewilayah kerja Bakorwil III Malang
-          </div>
+          <ul className="list-decimal ml-5 mb-8 text-sm md:text-base md:text-start text-justify">
+            <li>
+              Kegiatan koordinasi. fasilitasi, monitoring dan evaluasi kerja sama Blitar Raya, Pemerintah Kabupaten Malang dan Pemerintah Kabupaten Tulungagung dalam rangka mendukung Peraturan Presiden No. 80 Tahun 2019 tentang Proyek
+              Selingkar Wilis dan Jalan Lintas Selatan (JLS).
+            </li>
+            <li>
+              Rekomendasi Inisiasi percepatan pelaksanaan kesepakatan bersama pembangunan Bromo Tengger Semeru meliputi Pemerintah Provinsi Jawa Timur, Pemerintah Kota Batu, Pemerintah Kabupaten dan Kota Malang, Pemerintah Kabupaten dan
+              Kota Pasuruan, Pemerintah Kabupaten dan Kota Probolinggo dan Kabupaten Lumajang.
+            </li>
+            <li>Kegiatan koordinasi, fasilitasi, monitoring dan evaluasi kebencanaan daerah hulu dan hilir seputar Daerah Aliran Sungai (DAS) Brantas di Malang Raya.</li>
+            <li>Kegiatan koordinasi, fasilitasi, monitoring dan evaluasi pembinaan kearsipan mendukung pertanggungjawaban pengelolaan dana desa dalam penanganan COVID-19.</li>
+            <li>Kegiatan koordinasi, fasilitasi, monitoring dan evaluasi kesiapsiagaan menghadapi bencana hidrometeorologi (banjir, tanah longsor, dIl.) sewilayah kerja Bakorwil III Malang.</li>
+            <li>Kegiatan koordinasi, fasilitasi, monitoring dan evaluasi sistem kependudukan yang terintegrasi dengan Satu Data Jawa Timur untuk seluruh masyarakat sewilayah kerja Bakorwil III Malang</li>
+          </ul>
         </div>
       </div>
       {/* ================== BIDANG PEMBANGUNAN EKONOMI ================== */}
@@ -42,21 +40,19 @@ const Accordion = () => {
         <input type="radio" name="my-accordion-1" checked="checked" />
         <div className="collapse-title text-sm font-bold text-second flex items-center gap-2 md:text-lg">Bidang Pembangunan Ekonomi</div>
         <div className="collapse-content">
-          <p className="text-sm md:text-base">
-            1. Kegiatan koordinasi, fasilitasi, monitoring dan evaluasi inisiasi Memorandum of Understanding (MoU) kerja sama Malang Raya.
-            <br />
-            2. Kegiatan koordinasi, fasilitasi, monitoring dan evaluasi kerjasama pembangunan pariwisata dan ekonomi kreatif Malang Raya.
-            <br />
-            3. Kegiatan koordinasi, fasilitasi, monitoring dan evaluasi kerja sama platform digital sebagai media promosi karya talenta Millenial Job Center (MJC) Bakorwil III Malang.
-            <br />
-            4. Kegiatan koordinasi, fasilitasi, monitoring dan evaluasi proses sinkonisasi data pengembangan Website Geographic Information System (Web-GIS) Sistem Informasi Pembangunan Ekonomi (SIBANGEKO) di wilayah keria Bakorwil III
-            Malang.
-            <br />
-            5. Kegiatan koordinasi, fasilitasi, monitoring dan evaluasi penggalian potensi ekonomi baru di masa pandemi COVID-19.
-            <br />
-            6. Kegiatan koordinasi. fasilitasi. monitoring dan evaluasi pembinaan dan penguatan Himpunan Petani Pemakai Air (HIPPA) dan Gabungan Himpunan Petani Pemakai Air (GHIPPA) untuk kesejahteraan petani sewilayah keria Bakorwil III
-            Malang
-          </p>
+          <ul className="list-decimal ml-5 mb-8 text-sm md:text-base md:text-start text-justify">
+            <li>Kegiatan koordinasi, fasilitasi, monitoring dan evaluasi inisiasi Memorandum of Understanding (MoU) kerja sama Malang Raya.</li>
+            <li>Kegiatan koordinasi, fasilitasi, monitoring dan evaluasi kerjasama pembangunan pariwisata dan ekonomi kreatif Malang Raya.</li>
+            <li>Kegiatan koordinasi, fasilitasi, monitoring dan evaluasi kerja sama platform digital sebagai media promosi karya talenta Millenial Job Center (MJC) Bakorwil III Malang.</li>
+            <li>
+              Kegiatan koordinasi, fasilitasi, monitoring dan evaluasi proses sinkonisasi data pengembangan Website Geographic Information System (Web-GIS) Sistem Informasi Pembangunan Ekonomi (SIBANGEKO) di wilayah keria Bakorwil III ang.
+            </li>
+            <li>Kegiatan koordinasi, fasilitasi, monitoring dan evaluasi penggalian potensi ekonomi baru di masa pandemi COVID-19.</li>
+            <li>
+              Kegiatan koordinasi. fasilitasi. monitoring dan evaluasi pembinaan dan penguatan Himpunan Petani Pemakai Air (HIPPA) dan Gabungan Himpunan Petani Pemakai Air (GHIPPA) untuk kesejahteraan petani sewilayah keria Bakorwil III
+              Malang
+            </li>
+          </ul>
         </div>
       </div>
       {/* ================== BIDANG KEMASYARAKATAN ================== */}
@@ -64,20 +60,17 @@ const Accordion = () => {
         <input type="radio" name="my-accordion-1" checked="checked" />
         <div className="collapse-title text-sm font-bold text-second flex items-center gap-2 md:text-lg">Bidang Kemasyarakatan</div>
         <div className="collapse-content">
-          <p className="text-sm md:text-base">
-            1. Kegiatan koordinasi, fasilitasi, monitoring dan evaluasi efektivitas pembelajaran tatap muka bagi siswa sekolah sewilyah kerja Bakorwil III Malang
-            <br />
-            2. Kegiatan koordinasi, fasilitasi, monitoring dan evaluasi permasalahan tenaga kerja sewilayah kerja Bakorwil III Malang.
-            <br />
-            3. Kegiatan koordinasi, fasilitasi, monitoring dan evaluasi Kesejahteraan Sosial Anak dan Pemenuhan Hak Anak seta Perlindungan Khusus Anak menuju Provinsi Jawa Timur sebagai Provinsi Layak Anak (Provila) Tahun 2021.
-            <br />
-            4. Kegiatan koordinasi. fasilitasi, monitoring dan evaluasi Pembangunan Jaring Komunikasi dan Komunikasi antar OPD Ierkait Dalam Pelaksanaan Millenial Job Center.
-            <br />
-            5. Kegiatan koordinasi, fasilitasi, monitoring dan evaluasi optimalisasi vaksinasi COVID-19 untuk target Kekebalan Kelompok/Hera Immunity.
-            <br />
-            6. Kegiatan koordinasi, fasilitasi, monitoring dan evaluasi akselerasi implementasi Peraturan Gubernur No. 36 Tahun 2021 tentang Pelaksanaan Jaminan Sosial Tenaga Kerja Melalui BPJS Ketenagakerjaan Provinsi Jawa Timur di Kota
-            Surabaya, Kota Batu. Kota dan Kabupaten Blitar serta Kota dan Kabupaten Pasuruan
-          </p>
+          <ul className="list-decimal ml-5 mb-8 text-sm md:text-base md:text-start text-justify">
+            <li>Kegiatan koordinasi, fasilitasi, monitoring dan evaluasi efektivitas pembelajaran tatap muka bagi siswa sekolah sewilyah kerja Bakorwil III Malang</li>
+            <li>Kegiatan koordinasi, fasilitasi, monitoring dan evaluasi permasalahan tenaga kerja sewilayah kerja Bakorwil III Malang.</li>
+            <li>Kegiatan koordinasi, fasilitasi, monitoring dan evaluasi Kesejahteraan Sosial Anak dan Pemenuhan Hak Anak seta Perlindungan Khusus Anak menuju Provinsi Jawa Timur sebagai Provinsi Layak Anak (Provila) Tahun 2021.</li>
+            <li>Kegiatan koordinasi. fasilitasi, monitoring dan evaluasi Pembangunan Jaring Komunikasi dan Komunikasi antar OPD Ierkait Dalam Pelaksanaan Millenial Job Center.</li>
+            <li>Kegiatan koordinasi, fasilitasi, monitoring dan evaluasi optimalisasi vaksinasi COVID-19 untuk target Kekebalan Kelompok/Hera Immunity.</li>
+            <li>
+              Kegiatan koordinasi, fasilitasi, monitoring dan evaluasi akselerasi implementasi Peraturan Gubernur No. 36 Tahun 2021 tentang Pelaksanaan Jaminan Sosial Tenaga Kerja Melalui BPJS Ketenagakerjaan Provinsi Jawa Timur di Kota
+              Surabaya, Kota Batu. Kota dan Kabupaten Blitar serta Kota dan Kabupaten Pasuruan
+            </li>
+          </ul>
         </div>
       </div>
       {/* ================== BIDANG SARANA DAN PRASARANA ================== */}
@@ -85,19 +78,19 @@ const Accordion = () => {
         <input type="radio" name="my-accordion-1" checked="checked" />
         <div className="collapse-title text-sm font-bold text-second flex items-center gap-2 md:text-lg">Bidang Sarana Dan Prasarana</div>
         <div className="collapse-content">
-          <p className="text-sm md:text-base">
-            1. Kegiatan koordinasi, fasilitasi. monitoring dan evaluasi inisiasi perjanjian kerja sama pengelolaan lingkungan hidup Malang Raya.
-            <br />
-            2. Kegiatan koordinasi, fasilitasi, monitoring dan evaluasi Tempat Pembuangan Akhir (TPA) Sampah Regional.
-            <br />
-            3. Kegiatan koordinasi, fasilitasi, monitoring dan evaluasi permasalahan pengelolaan sampah di kawasan Sungai Welang Kabupaten Pasuruan.
-            <br />
-            4. Kegiatan koordinasi, fasilitasi, monitoring dan evaluasi laporan progres pembangunan Jalan, Lintas Selatan (JLS) di wilayah Kabupaten Blitar.
-            <br />
-            5. Kegiatan koordinasi. fasilitasi, monitoring dan evaluasi laporan progres pembangunan Jalan Lints Selatan (JLS) di wilayah Kabupaten Malang.
-            <br />
-            6. Kegiatan koordinasi, fasilitasi, monitoring dan evaluasi laporan progres pembangunan Jalan Lintas Selatan (JLS) di wilayah Kabupaten Blitar jilid 2.
-          </p>
+          <ul className="list-decimal ml-5 mb-8 text-sm md:text-base md:text-start text-justify">
+            <li>Kegiatan koordinasi, fasilitasi. monitoring dan evaluasi inisiasi perjanjian kerja sama pengelolaan lingkungan hidup Malang Raya.</li>
+
+            <li>Kegiatan koordinasi, fasilitasi, monitoring dan evaluasi Tempat Pembuangan Akhir (TPA) Sampah Regional.</li>
+
+            <li>Kegiatan koordinasi, fasilitasi, monitoring dan evaluasi permasalahan pengelolaan sampah di kawasan Sungai Welang Kabupaten Pasuruan.</li>
+
+            <li>Kegiatan koordinasi, fasilitasi, monitoring dan evaluasi laporan progres pembangunan Jalan, Lintas Selatan (JLS) di wilayah Kabupaten Blitar.</li>
+
+            <li>Kegiatan koordinasi. fasilitasi, monitoring dan evaluasi laporan progres pembangunan Jalan Lints Selatan (JLS) di wilayah Kabupaten Malang.</li>
+
+            <li>Kegiatan koordinasi, fasilitasi, monitoring dan evaluasi laporan progres pembangunan Jalan Lintas Selatan (JLS) di wilayah Kabupaten Blitar jilid 2.</li>
+          </ul>
         </div>
       </div>
     </div>
